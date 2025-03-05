f3cc9cd7ab2be2dd1857e7899ff1438957d4cac2
yerim1ee <yerim1226@gmail.com>
2025-03-06 08:31:33 +0900

    fix: jpa auditing 설정 추가 [PRBE-27]

--- src/main/java/com/olive/pribee/PribeeApplication.java
+++ src/main/java/com/olive/pribee/PribeeApplication.java
@@ -2,9 +2,11 @@ package com.olive.pribee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories
+@EnableJpaAuditing
 @SpringBootApplication
 public class PribeeApplication {
 
