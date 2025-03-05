77b9e717b194e49fc819f34d08c6cd096e8954b2
yerim1ee <yerim1226@gmail.com>
2025-03-05 15:15:17 +0900

    feat: facebook accessToken api 응답 dto 추가 [PRBE-15]

---
+++ src/main/java/com/olive/pribee/module/auth/dto/res/FacebookTokenRes.java
@@ -0,0 +1,27 @@
+package com.olive.pribee.module.auth.dto.res;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FacebookTokenRes {
+	private String accessToken;
+	private String tokenType;
+	private long expiresIn;
+
+	@Builder
+	public FacebookTokenRes(
+		@JsonProperty("access_token") String accessToken,
+		@JsonProperty("token_type") String tokenType,
+		@JsonProperty("expires_in") long expiresIn
+	) {
+		this.accessToken = accessToken;
+		this.tokenType = tokenType;
+		this.expiresIn = expiresIn;
+	}
+}
