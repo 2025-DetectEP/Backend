82ef35995c9b77773b61c9ac4f56d3be93e3ce18
yerim1ee <yerim1226@gmail.com>
2025-03-07 15:31:08 +0900

    feat: DLP api 요청하기 위한 dto 추가 [PRBE-35]

---
+++ src/main/java/com/olive/pribee/infra/api/google/dlp/dto/req/DlpReq.java
@@ -0,0 +1,61 @@
+package com.olive.pribee.infra.api.google.dlp.dto.req;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.olive.pribee.global.enums.DetectKeyword;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.olive.pribee.global.util.fileUtil.encodeImageToBase64;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public record DlpReq(
+	@JsonProperty("item") Item item,
+	@JsonProperty("inspectConfig") InspectConfig inspectConfig
+) {
+	public static DlpReq ofText(String value) {
+		DlpReq req = new DlpReq(
+			new Item(value),
+			new InspectConfig(
+				DetectKeyword.getInfoTypes().stream().map(InfoType::new).toList(),
+				"POSSIBLE"
+			)
+		);
+		return req;
+	}
+
+	public static DlpReq ofImage(String imageUrl) {
+		String base64Image = encodeImageToBase64(imageUrl);
+		return new DlpReq(
+			new Item(Map.of("type", "IMAGE", "data", base64Image)),
+			new InspectConfig(
+				DetectKeyword.getInfoTypes().stream().map(InfoType::new).toList(),
+				"POSSIBLE"
+			)
+		);
+	}
+}
+
+record Item(@JsonProperty("value") String value, @JsonProperty("byteItem") Map<String, String> byteItem) {
+	public Item(String value) {
+		this(value, null);
+	}
+	public Item(Map<String, String> byteItem) {
+		this(null, byteItem);
+	}
+}
+
+record InspectConfig(
+	@JsonProperty("infoTypes") List<InfoType> infoTypes,
+	@JsonProperty("minLikelihood") String minLikelihood,
+	@JsonProperty("limits") Map<String, Integer> limits,
+	@JsonProperty("includeQuote") boolean includeQuote
+) {
+	public InspectConfig(List<InfoType> infoTypes, String minLikelihood) {
+		this(infoTypes, minLikelihood, Map.of("maxFindingsPerItem", 0), true);
+	}
+}
+
+record InfoType(@JsonProperty("name") String name) {}
